fb8dd138c591a09e6aa9cb553a79ffeb24af3dc2
BharaniEswaran <41111057+BharaniEswaran@users.noreply.github.com>
2018-07-12 10:03:03 +0530

    Create postiveng.java

---
+++ postiveng.java
@@ -0,0 +1,15 @@
+pubilc class Postiveng{
+public static void main(String [] args){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+if(n==0){
+System.out.print("Zero");
+}
+else if(n>0){
+System.out.print("positive");
+}
+else{
+System.out.print("Negative");
+}
+}
+}
