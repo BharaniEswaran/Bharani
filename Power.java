a26f2eff8b10ee7624b4920a90af48844f033dc5
BharaniEswaran <41111057+BharaniEswaran@users.noreply.github.com>
2018-07-13 18:06:31 +0530

    Create Power.java

---
+++ Power.java
@@ -0,0 +1,12 @@
+public class Power{
+public static void main(String [] args){
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int b=sc.nextInt();
+int h=1;
+for(int i=0;i<b;i++){
+h=h*a;
+}
+System.out.print(h);
+}
+}
