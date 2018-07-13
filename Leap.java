5e98f7281b3a234757dd2c426cde0804cf600366
BharaniEswaran <41111057+BharaniEswaran@users.noreply.github.com>
2018-07-13 17:49:44 +0530

    Create Leap.java

---
+++ Leap.java
@@ -0,0 +1,12 @@
+public class Leap{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+if(a%4==0){
+System.out.print("Leap year");
+}
+else{
+system.out.print("not a leap year");
+}
+}
+}
