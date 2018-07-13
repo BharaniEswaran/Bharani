67d27153de6815a90a128811e734fd90bb49dd6b
BharaniEswaran <41111057+BharaniEswaran@users.noreply.github.com>
2018-07-13 17:59:17 +0530

    Create SumNew.java

---
+++ SumNew.java
@@ -0,0 +1,16 @@
+public class SumNew{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int n=sc.next int();
+int []arr=new int[n];
+for(int i=1;i<=n;i++){
+arr[i]=sc.nextInt();
+}
+int a=sc.nextInt();
+int sum=0;
+for(int i=1;i<=a;i++){
+sum+=i;
+}
+System.out.print(sum);
+}
+}
