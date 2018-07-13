7be4ee229ab3258a2093c305d2063ddcc343dc81
BharaniEswaran <41111057+BharaniEswaran@users.noreply.github.com>
2018-07-13 17:43:00 +0530

    Create Charac.java

---
+++ Charac.java
@@ -0,0 +1,19 @@
+public class Prp8 {
+	public static void main(String[] args){
+		Scanner sv=new Scanner(System.in);
+		char ch=sv.next().charAt(0);
+	
+		
+		if(( ch>=65 && ch<=90) ||(ch>=97 && ch<=122)){
+		
+			System.out.println("Alphabets");	
+			
+		}
+		
+		
+		
+		else{
+			System.out.print("not a characters");
+      }
+		}
+	}
