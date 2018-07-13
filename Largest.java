e6fc6a78529901c0b010b80505396b9d3d3c7271
BharaniEswaran <41111057+BharaniEswaran@users.noreply.github.com>
2018-07-13 17:46:57 +0530

    Create Largest.java

---
+++ Largest.java
@@ -0,0 +1,17 @@
+public class Largest{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int a=sc.nextint();
+int b=sc.nextInt();
+int c=sc.nextInt();
+if(a>b && a>C){
+System.out.print(a);
+}
+else if(b>c && b>a){
+System.out.print(b);
+}
+else{
+System.out.print(c);
+}
+}
+}
