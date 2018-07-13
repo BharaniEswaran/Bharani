5ae931b8ffba08e604af3df7932fecc3eea1e4a6
BharaniEswaran <41111057+BharaniEswaran@users.noreply.github.com>
2018-07-13 18:02:20 +0530

    Create Count.java

---
+++ Count.java
@@ -0,0 +1,8 @@
+public class Count{
+public static void main(String[] args){
+scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int len=Integer.toString(n).length();
+System.out.print(len);
+}
+}
